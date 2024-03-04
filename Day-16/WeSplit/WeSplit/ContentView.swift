13f7b8cd0dacfbb5afedc99112982db751955dc1
TripToCodes <iam.somj@gmail.com>
2024-03-04 12:03:44 -0800

    Add another tip option

--- Day-16/WeSplit/WeSplit/ContentView.swift
+++ Day-16/WeSplit/WeSplit/ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     let tipPercentages: [Int] = [15, 18, 20, 25, 0]
     @State private var selectedTip: Int = 18 // @State property wrapper is used to create a two-way binding
+    @State private var writtenTip: Int = 0
     @State private var checkAmount: Double = 0.0
     @State private var numberOfPeople: Int = 2
     @State private var totalAmount: Double = 0.0
@@ -16,25 +17,28 @@ struct ContentView: View {
             Form {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
-                        .keyboardType(.decimalPad)
+                    .keyboardType(.decimalPad)
                 }
 
                 Section(header: Text("Number of people")) {
                     TextField("Number of people", value: $numberOfPeople, format: .number)
 
                 }
-                
+
                 Section(header: Text("Tip Percentage")) {
                     Picker("Tips", selection: $selectedTip) {
                         ForEach(tipPercentages, id: \.self) { tip in
-                            Text("\(tip)%")
+                        Text("\(tip)%")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
+
+                    TextField("Tip Percentage", value: writtenTip != 0 ? $writtenTip : $selectedTip, format: .number) // If writtenTip is not 0, then use writtenTip, else use selectedTip
+                    .keyboardType(.decimalPad) 
                 }
                 
                 Section(header: Text("Total per person")) {
-                    Text(String(format: "%.2f", splitbill + (splitbill * Double(selectedTip) / 100)))
+                    Text(String(format: "%.2f", splitbill + (splitbill * Double(selectedTip) / 100))) // %.2f means a floating-point number with two digits after the decimal point
                 }
             }
             .navigationBarTitle("WeSplit")
