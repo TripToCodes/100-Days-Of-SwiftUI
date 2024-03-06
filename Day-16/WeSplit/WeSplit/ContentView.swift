f6e4f136c2c207c6304ad07bee0e3ed47a59cc7a
TripToCodes <iam.somj@gmail.com>
2024-03-05 16:09:41 -0800

    Refactor and improve other tip option

--- Day-16/WeSplit/WeSplit/ContentView.swift
+++ Day-16/WeSplit/WeSplit/ContentView.swift
@@ -1,47 +1,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    let tipPercentages: [Int] = [15, 18, 20, 25, 0]
-    @State private var selectedTip: Int = 18 // @State property wrapper is used to create a two-way binding
+    let tipPercentages: [String] = ["15%", "18%", "20%", "25%", "Other"]
+    @State private var selectedTipIndex: Int = 0 // @State property wrapper is used to create a two-way binding
     @State private var writtenTip: Int = 0
-    @State private var checkAmount: Double = 0.0
-    @State private var numberOfPeople: Int = 2
+    @State private var checkAmountText: String = ""
+    @State private var numberOfPeopleText: String = ""
     @State private var totalAmount: Double = 0.0
+
+    var checkAmount: Double {
+        return Double(checkAmountText) ?? 0
+    }
     
-    var splitbill: Double {
+    var numberOfPeople: Int {
+        return Int(numberOfPeopleText) ?? 1
+    }
+    
+    var splitBill: Double {
         return checkAmount / Double(numberOfPeople)
     }
     
     var body: some View {
         NavigationView {
-            Form {
-                Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
-                    .keyboardType(.decimalPad)
-                }
-
-                Section(header: Text("Number of people")) {
-                    TextField("Number of people", value: $numberOfPeople, format: .number)
-
+          Form {
+                Section(header: Text("Enter the bill details")) {
+                    TextField("Amount", text: $checkAmountText)
+                        .keyboardType(.decimalPad)
+                    TextField("Number of people", text: $numberOfPeopleText)
+                        .keyboardType(.numberPad)
                 }
 
                 Section(header: Text("Tip Percentage")) {
-                    Picker("Tips", selection: $selectedTip) {
-                        ForEach(tipPercentages, id: \.self) { tip in
-                        Text("\(tip)%")
+                    Picker("Tips", selection: $selectedTipIndex) {
+                        ForEach(0..<tipPercentages.count, id: \.self) { index in
+                            Text(tipPercentages[index])
+                                .tag(index)
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
-
-                    TextField("Tip Percentage", value: writtenTip != 0 ? $writtenTip : $selectedTip, format: .number) // If writtenTip is not 0, then use writtenTip, else use selectedTip
-                    .keyboardType(.decimalPad) 
+                    
+                    // if selectedTip is Other, show TextField
+                    if selectedTipIndex == tipPercentages.count - 1 {
+                        TextField("Tip Percentage", value: $writtenTip, format: .number)
+                            .keyboardType(.numberPad)
+                    }
                 }
                 
                 Section(header: Text("Total per person")) {
-                    Text(String(format: "%.2f", splitbill + (splitbill * Double(selectedTip) / 100))) // %.2f means a floating-point number with two digits after the decimal point
+                    Text(String(format: "%.2f", splitBill + (splitBill * Double(getSelectedTip()) / 100))) // %.2f means a floating-point number with two digits after the decimal point
                 }
             }
             .navigationBarTitle("WeSplit")
+        }
+    }
+    
+    func getSelectedTip() -> Int {
+        if selectedTipIndex == tipPercentages.count - 1 {
+            return writtenTip
+        } else {
+            return Int(tipPercentages[selectedTipIndex].dropLast()) ?? 0
         }
     }
 }
