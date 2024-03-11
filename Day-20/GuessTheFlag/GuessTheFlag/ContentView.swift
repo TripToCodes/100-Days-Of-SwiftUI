63ae11edb81d8d455f1ca2f1b5e69a36085d9473
TripToCodes <iam.somj@gmail.com>
2024-03-11 14:09:12 -0700

    Create Day 20 GuessTheFlag project

---
+++ Day-20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Somi Jeon on 2024-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day-20/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Somi Jeon on 2024-03-11.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
